b422d858714e7b3e604da62ee24fd20abb24c7da
cannedyeti <cpotebnya@gmail.com>
2024-05-15 12:49:44 -0700

    init fetch

--- src/features/fetch-data/FetchData.jsx
+++ src/features/fetch-data/FetchData.jsx
@@ -1,39 +1,15 @@
-const RIOT_VERSION_API =
-  "https://ddragon.leagueoflegends.com/api/versions.json";
-const getChampDataApi = (version) =>
-  `https://ddragon.leagueoflegends.com/cdn/${version}/data/en_US/champion.json`;
-const getItemDataApi = (version) =>
-  `https://ddragon.leagueoflegends.com/cdn/${version}/data/en_US/item.json`;
-
-const fetchLeagueVersion = async () => {
-  const res = await fetch(RIOT_VERSION_API);
-  const versionJson = await res.json();
-  return versionJson[0];
-};
-
-const fetchLeagueData = async () => {
-  let riotData = {};
-  const version = await fetchLeagueVersion();
-  riotData.version = version;
-
-  const champData = await fetch(getChampDataApi(version)).then((res) =>
-    res.json()
-  );
-  riotData.champions = champData?.data;
-
-  const itemData = await fetch(getItemDataApi(version)).then((res) =>
-    res.json()
-  );
-  riotData.items = itemData?.data;
-
-  console.log({ riotData });
+const getData = async () => {
+  const data = await fetch(
+    "https://ultimate-bravery-v3-data.s3.us-west-2.amazonaws.com/data.json"
+  ).then((x) => x.json());
+  console.log("test", data);
 };
 
 export function FetchData() {
   return (
     <>
       <h2>Fetch data</h2>
-      <button onClick={fetchLeagueData}>Click this shit</button>
+      <button onClick={getData}>Click this shit</button>
     </>
   );
 }
